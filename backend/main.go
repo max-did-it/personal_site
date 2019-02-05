d8038b7e8dfdca2f9bdc970553ea1169164d1577
Maxim Bezlegkiy <jesus.dead.drunk@gmail.com>
2019-02-05 14:08:59 +0800

    Add mailer
    Former-commit-id: e739cd8ddb7e9cbe2a7bc2d43f619bfdd138e675

---
+++ backend/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"gitlab.com/SiberianPanda/selfcard_service/src/pkg/server"
+)
+
+func main() {
+	server.Up(":8080")
+	// fmt.Scanln()
+}
---
+++ backend/src/pkg/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"gitlab.com/SiberianPanda/selfcard_service/src/pkg/mail"
+)
+
+func mailHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var RawAnswer = make(map[string]bool, 1)
+		switch err := mail.Send(r); err {
+		case nil:
+			RawAnswer["success"] = true
+		default:
+			log.Print(err)
+			RawAnswer["success"] = false
+		}
+		var rawDataInterface interface{} = RawAnswer
+		data, _ := json.Marshal(rawDataInterface)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+		http.Redirect(w, r, "/", 301)
+		log.Print("redirected\n")
+	default:
+		http.Redirect(w, r, "/", 301)
+	}
+}
+
+func routerSet() {
+	http.HandleFunc("/callback", mailHandler)
+}
+
+// Up starting http server
+func Up(port string) {
+
+	log.Println("starting server at :8080")
+	routerSet()
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Println("Server failed with err -> " + err.Error())
+	}
+}
